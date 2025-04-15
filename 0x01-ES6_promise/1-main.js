b37837aeb5774f4d8d3a301ee70700b1c15f7f9d
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-04-15 23:17:29 +0300

    4-user-promise.js

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,5 +1,11 @@
 import getResponseFromAPI from "./0-promise.js";
 import getFullResponseFromAPI from './1-promise';
+import handleProfileSignup from "./3-all";
+import signUpUser from "./4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
+
+handleProfileSignup();
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
